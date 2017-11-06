ca3aa58f109f48141a453ada5171a93de5d408b7
James Weng <wengj1>
2017-11-06 11:26:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/wengj1/sqlquiz/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.wengj1.sqlquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
