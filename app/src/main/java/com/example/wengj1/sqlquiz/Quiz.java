eb158a4e71eb9e9ed627382d3b8d0ef1a28d8fda
James Weng <wengj1@bxscience.edu>
2017-11-09 11:34:04 -0500

    Continue SQLite and etc.

--- app/src/main/java/com/example/wengj1/sqlquiz/Quiz.java
+++ app/src/main/java/com/example/wengj1/sqlquiz/Quiz.java
@@ -1,12 +1,36 @@
 package com.example.wengj1.sqlquiz;
 
+import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 
-@Entity
+@Entity(tableName = "questions")
 public class Quiz {
-    @PrimaryKey
-    private int questionID;
+    @PrimaryKey(autoGenerate = true)
+    private int qID;
 
+    @ColumnInfo(name = "question")
+    private String question;
 
+    @ColumnInfo(name = "answer")
+    private String answer;
+
+    @ColumnInfo(name = "category_ID")
+    private int catID;
+
+    public int getqID() {
+        return qID;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public int getCatID() {
+        return catID;
+    }
 }
