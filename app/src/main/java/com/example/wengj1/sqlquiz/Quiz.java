5643602380001e99a64c2c1d5eb04be90e499719
James Weng <wengj1@bxscience.edu>
2017-11-09 03:23:41 -0500

    Start SQL and optimize code.

--- app/src/main/java/com/example/wengj1/sqlquiz/Quiz.java
+++ app/src/main/java/com/example/wengj1/sqlquiz/Quiz.java
@@ -3,10 +3,6 @@ package com.example.wengj1.sqlquiz;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 
-/**
- * Created by wengj1 on 11/8/2017.
- */
-
 @Entity
 public class Quiz {
     @PrimaryKey
