98bfb715fdabca8ea0d8db5cf8a41835e87d55c1
Your NameProjectBM <quderek@yahoo.com>
2017-11-11 21:13:22 -0500

    Minor changes to Game.java

--- app/src/main/java/com/example/wengj1/sqlquiz/Game.java
+++ app/src/main/java/com/example/wengj1/sqlquiz/Game.java
@@ -3,9 +3,11 @@ package com.example.wengj1.sqlquiz;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -14,6 +16,7 @@ public class Game extends AppCompatActivity {
     private ImageButton previous, next;
     private Button submit;
     private RadioGroup radioGroup;
+    private RadioButton A, B, C, D, E;
     private ScrollView scroll;
     private TextView question;
     private int count;
@@ -25,18 +28,36 @@ public class Game extends AppCompatActivity {
 
         String cate = getIntent().getStringExtra("category");
 
-        previous = (ImageButton) findViewById(R.id.previous);
-        next = (ImageButton) findViewById(R.id.next);
-        submit = (Button) findViewById(R.id.answer);
-        radioGroup = (RadioGroup) findViewById(R.id.radiogroup);
-        question = (TextView) findViewById(R.id.question);
-        //scroll = (ScrollView)findViewById(R.id.scrollText);
+        submit = findViewById(R.id.answer);
+        radioGroup = findViewById(R.id.radiogroup);
+        question = findViewById(R.id.question);
+        A = findViewById(R.id.choice1);
+        B = findViewById(R.id.choice2);
+        C = findViewById(R.id.choice3);
+        D = findViewById(R.id.choice4);
+        E = findViewById(R.id.choice5);
         count = 0;
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 count += 1;
+                int btID = radioGroup.getCheckedRadioButtonId();
+                switch (btID) {
+                    case R.id.choice1:
+                        break;
+                    case R.id.choice2:
+                        break;
+                    case R.id.choice3:
+                        break;
+                    case R.id.choice4:
+                        break;
+                    case R.id.choice5:
+                        break;
+                    default:
+                        Log.d("Checked Button", "No work");
+                        break;
+                }
                 change();
             }
         });
