3137f045bb11547742ab582a4d6c7562de5ee599
James Weng <wengj1@bxscience.edu>
2017-11-12 13:17:47 -0500

    Modify MainActivity

--- app/src/main/java/com/example/wengj1/sqlquiz/MainActivity.java
+++ app/src/main/java/com/example/wengj1/sqlquiz/MainActivity.java
@@ -1,24 +1,21 @@
-package com.example.wengj1.sqlquiz;
+package com.example.wengj1.sqlquiz
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
 
-public class MainActivity extends AppCompatActivity {
-    private Button play;
+class MainActivity : AppCompatActivity() {
+    private var play: Button? = null
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        play = (Button) findViewById(R.id.play);
-        play.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,Categories.class);
-                startActivity(intent);
-            }
-        });
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        play = findViewById<View>(R.id.play) as Button
+        play!!.setOnClickListener {
+            val intent = Intent(this@MainActivity, Categories::class.java)
+            startActivity(intent)
+        }
     }
 }
