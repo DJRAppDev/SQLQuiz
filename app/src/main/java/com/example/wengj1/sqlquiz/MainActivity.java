2b2a4293242fd4c9df221c337308dd5c4f117411
James Weng <wengj1@bxscience.edu>
2017-11-13 10:02:11 -0500

    Fix accidentally broken Java/Kotlin code.

--- app/src/main/java/com/example/wengj1/sqlquiz/MainActivity.java
+++ app/src/main/java/com/example/wengj1/sqlquiz/MainActivity.java
@@ -1,21 +1,25 @@
-package com.example.wengj1.sqlquiz
+package com.example.wengj1.sqlquiz;
 
-import android.content.Intent
-import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.view.View
-import android.widget.Button
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-class MainActivity : AppCompatActivity() {
-    private var play: Button? = null
+public class MainActivity extends AppCompatActivity {
+    private Button play;
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        play = findViewById<View>(R.id.play) as Button
-        play!!.setOnClickListener {
-            val intent = Intent(this@MainActivity, Categories::class.java)
-            startActivity(intent)
-        }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        play = findViewById(R.id.play);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Categories.class);
+                startActivity(intent);
+            }
+        });
     }
 }
