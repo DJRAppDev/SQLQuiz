30220951e004e478d3e0dcaa74418bde39ce8e64
James Weng <wengj9@gmail.com>
2017-11-18 19:34:07 -0500

    Fix SQL crash when restarting app.

--- app/src/main/java/com/example/wengj1/sqlquiz/End.java
+++ app/src/main/java/com/example/wengj1/sqlquiz/End.java
@@ -1,5 +1,6 @@
 package com.example.wengj1.sqlquiz;
 
+import android.arch.persistence.room.Room;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -8,11 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.fstyle.library.helper.AssetSQLiteOpenHelperFactory;
+
 public class End extends AppCompatActivity {
     Button exit;
     Button review;
     TextView score;
     MediaPlayer victorymusic;
+    private AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,7 @@ public class End extends AppCompatActivity {
         exit = findViewById(R.id.exitButton);
         review = findViewById(R.id.reviewButton);
         score = findViewById(R.id.score);
+        db = Room.databaseBuilder(getApplicationContext(),AppDatabase.class,"quiz.db").openHelperFactory(new AssetSQLiteOpenHelperFactory()).allowMainThreadQueries().fallbackToDestructiveMigration().build();
 
         String correct = getIntent().getStringExtra("Answer");
 
@@ -32,6 +37,7 @@ public class End extends AppCompatActivity {
         exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                db.quizDao().eraseAns();
                 finish();
                 System.exit(0);
             }
