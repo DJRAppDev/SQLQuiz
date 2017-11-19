06fafbba6107ae9ea33032a7a92e9f98d6de85bd
Your NameProjectBM <quderek@yahoo.com>
2017-11-18 19:34:36 -0500

    More things to review.java

--- app/src/main/java/com/example/wengj1/sqlquiz/End.java
+++ app/src/main/java/com/example/wengj1/sqlquiz/End.java
@@ -42,7 +42,7 @@ public class End extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(End.this,Review.class);
-                intent.putExtra("Category",category);
+                intent.putExtra("Category",category+"");
                 startActivity(intent);
             }
         });
